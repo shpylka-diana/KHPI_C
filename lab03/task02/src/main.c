300d263a5db16fdf94e137967eaa09178558d953
Diana <73955330+shpylka-diana@users.noreply.github.com>
2020-11-29 20:23:41 +0200

    lab03

---
+++ lab03/task02/src/main.c
@@ -0,0 +1,12 @@
+        #define NUM 456
+        int main()
+        {
+            int a = NUM / 100; // 4
+            int b = NUM / 10 % 10; // 5
+            int c = NUM % 10; // 6
+            int result = (c * 100) + (b * 10) + a; //654
+
+
+
+        return 0;
+        }
