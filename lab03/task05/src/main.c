5f0726279a30b6e63da2c81527656ddafca733c5
Diana <73955330+shpylka-diana@users.noreply.github.com>
2020-11-29 21:06:10 +0200

    lab03

--- lab03/task05/src/main.c
+++ lab03/task05/src/main.c
@@ -1,10 +1,10 @@
 #define NUM 546
 int main()
 {
-    float a = NUM / 100; // первое число
+    float a = NUM / 100; // первое число 5
     float b = NUM % 10; // третье число
-    float c = a / b; // во сколько раз первая цифра больше третьей
-    float result = ((float)(((int)(c * 100)) / 100.0)); // обрезання до второй цифры
+    float c = a / b; 
+    float result = ((float)(((int)(c * 100)) / 100.0));
 
 return 0;
 }
